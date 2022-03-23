fa5d4f775d9a437a668d0211506e55a946ac1fa8
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-23 10:14:12 +0800

    deploy: fix config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-xvite-web' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-xvite-web/' : '/', // vue-xvite-web
 }
