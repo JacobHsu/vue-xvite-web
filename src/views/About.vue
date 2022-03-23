90c666c9ef14702534d6f30b0fdf226fa2237455
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-23 14:21:18 +0800

    feat: scss

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
+  <div class="not-found">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.not-found {
+    width: 100%;
+    text-align: center;
+    font-size: 40px;
+    margin-top: 200px;
+}
+</style>
